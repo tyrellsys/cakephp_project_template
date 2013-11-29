0f0bacd4a2a70be98a01477fd8c9d22c47db0328
mtak3 <mtak@tyrellsys.com>
2013-11-29 18:00:22 +0900

    app/Console/cake.phpのCakePHP本体の位置を変更しました。

--- app/Console/cake.php
+++ app/Console/cake.php
@@ -26,7 +26,7 @@ if (function_exists('ini_set')) {
 
 	// the following line differs from its sibling
 	// /app/Console/cake.php
-	ini_set('include_path', $root . PATH_SEPARATOR .  $ds . 'usr' . $ds . 'local' . $ds . 'cakephp' . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+	ini_set('include_path', $root . PATH_SEPARATOR .  $root . $ds . 'Vendor' . DS . 'pear-pear.cakephp.org' . DS . 'CakePHP' . PATH_SEPARATOR . ini_get('include_path'));
 }
 
 if (!include ($dispatcher)) {
