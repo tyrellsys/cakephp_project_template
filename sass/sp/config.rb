1cc9b0f0d77683655668cd5403a3f2e5e453873e
mtak3 <mtak@tyrellsys.com>
2014-01-23 15:04:02 +0900

    sass用ディレクトリの作成
    mkdir sass
    cd sass
    vi Gemfile
    bundle install
    rbenv rehash
    mkdir pc sp
    vi pc/config.rb
    vi sp/config.rb
    cd pc
    compass init
    cd sp
    compass init
    を行っています。

---
+++ sass/sp/config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "../../app/webroot/assets/sp/"
+css_dir = "../../app/webroot/assets/sp/css/"
+sass_dir = "scss"
+images_dir = "../../app/webroot/assets/sp/images/"
+sprite_load_path = "sprites"
+# javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+cache = false
